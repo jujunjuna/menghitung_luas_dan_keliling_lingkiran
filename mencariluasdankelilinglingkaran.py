f62c41fb4566cbef98cc4df2860fe9d221a9de8b
jujunjuna <jujunn.0698@gmail.com>
2021-11-01 22:01:35 +0700

    memasukan file

---
+++ mencariluasdankelilinglingkaran.py
@@ -0,0 +1,8 @@
+#diketahui lingkaran berjari jari 7 cm
+r = 7
+π = 22 / 7
+luas_lingkaran = r * r * π
+print ('luas lingkaran = ',luas_lingkaran)
+
+keliling_lingkaran = 2 * π * r
+print ('keliling lingkaran =',keliling_lingkaran)
